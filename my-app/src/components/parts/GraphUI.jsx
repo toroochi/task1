aa8d0d5ac99d926031d903e8b116cf6e429b796f
toroochi <kanon.0982@gmail.com>
2024-04-10 16:00:05 +0900

    add : task7/task2highlight

---
+++ my-app/src/components/parts/GraphUI.jsx
@@ -0,0 +1,16 @@
+import './style.css';
+import React from 'react';
+
+function Graph() {
+    return(
+        <div>
+            <table>
+                <tr>
+                    <th></th>
+                </tr>
+            </table>
+        </div>
+    )
+};
+
+export default Graph;
